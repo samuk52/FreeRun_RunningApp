d6a35566f595d7046c0fb6ddca60115214cc0965
samuk52 <samuele.corallo10@gmail.com>
2024-05-28 22:58:37 +0200

    added method for retrieving user information from the database

--- app/src/main/java/it/insubria/freerun_runningapp/Managers/DatabaseManager.kt
+++ app/src/main/java/it/insubria/freerun_runningapp/Managers/DatabaseManager.kt
@@ -2,19 +2,29 @@ package it.insubria.freerun_runningapp.Managers
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.firestore
 
 class DatabaseManager {
 
     private val db = Firebase.firestore
+    private val authManager = AuthenticationManager()
+
 
     // funzione che aggiunge un nuovo utente al database.
-    fun addNewUserToDB(name: String, weight: Float): Task<DocumentReference> {
+    fun addNewUserToDB(name: String, weight: Float, gender: String): Task<Void> {
         // TODO vedere se fare classe user.
-        val user = mapOf("name" to name, "weight" to weight)
-        return db.collection("users")
-            .add(user)
+        val user = mapOf("name" to name, "weight" to weight, "gender" to gender)
+        return db.collection("users").document(authManager.getCurrentUser()!!.uid)
+            .set(user)
+    }
+
+    // funzione che restituisce le informazioni dell'utente loggato
+    fun getUserInfo(): Task<DocumentSnapshot>{
+        //DEBUG todo remove
+        println("User id -> ${authManager.getCurrentUser()!!.uid}")
+        return db.collection("users").document(authManager.getCurrentUser()!!.uid)
+            .get()
     }
 
 }
