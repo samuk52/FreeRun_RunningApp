df096fbd475aafe3b76c0b76911de93d36e0f57d
samuk52 <samuele.corallo10@gmail.com>
2024-05-28 22:59:34 +0200

    Added weight retrieval from database

--- app/src/main/java/it/insubria/freerun_runningapp/TrackingRunComponents/CaloriesCounter.kt
+++ app/src/main/java/it/insubria/freerun_runningapp/TrackingRunComponents/CaloriesCounter.kt
@@ -1,10 +1,23 @@
 package it.insubria.freerun_runningapp.TrackingRunComponents
 
+import it.insubria.freerun_runningapp.Managers.DatabaseManager
+
 class CaloriesCounter {
 
     // TODO recuperare peso dell'utente dal database
-    val weight: Float = 70.0f // peso per debug
+    private val databaseManager = DatabaseManager()
+    private var weight = 0f
 
+    init {
+        databaseManager.getUserInfo().addOnSuccessListener { document ->
+            if(document.getDouble("weight") != null) {
+                weight = document.getDouble("weight")!!.toFloat()
+                println("WEIGHT -> $weight")
+            }
+        }
+    }
+
+    // funzione che calcola le calorie consumato dall'utente
     fun getCalories(km: Float): Int{
         return (weight * km * 0.9).toInt()
     }
