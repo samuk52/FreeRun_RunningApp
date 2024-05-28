45fa9d8abdc68f1f77f0aab3f68d65459cba5d0b
samuk52 <samuele.corallo10@gmail.com>
2024-05-28 22:57:53 +0200

    updated minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "it.insubria.freerun_runningapp"
-        minSdk = 24
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
