7fcadd9ad63e6a8e0721f018a8ba5028a59bd1f0
samuk52 <samuele.corallo10@gmail.com>
2024-05-29 17:45:26 +0200

    added in the databaseManager the method that adds a new run in the database

--- app/src/main/java/it/insubria/freerun_runningapp/Managers/DatabaseManager.kt
+++ app/src/main/java/it/insubria/freerun_runningapp/Managers/DatabaseManager.kt
@@ -1,5 +1,6 @@
 package it.insubria.freerun_runningapp.Managers
 
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.DocumentSnapshot
@@ -14,9 +15,24 @@ class DatabaseManager {
     // funzione che aggiunge un nuovo utente al database.
     fun addNewUserToDB(name: String, weight: Float, gender: String): Task<Void> {
         // TODO vedere se fare classe user.
-        val user = mapOf("name" to name, "weight" to weight, "gender" to gender)
-        return db.collection("users").document(authManager.getCurrentUser()!!.uid)
-            .set(user)
+        val user = mapOf(
+            "name" to name,
+            "weight" to weight,
+            "gender" to gender
+        )
+        return db.collection("users").document(authManager.getCurrentUser()!!.uid).set(user)
+    }
+
+    // metodo che aggiunge nel database una nuova corsa (activity)
+    fun addNewActivity(time: String, distance: String, avgPace: String, calories: String, locations: ArrayList<LatLng>){
+        val activity = mapOf(
+            "time" to time,
+            "distance" to distance,
+            "avgPace" to avgPace,
+            "calories" to calories,
+            "locations" to locations
+        )
+        db.collection("users").document(authManager.getCurrentUser()!!.uid).collection("activities").add(activity)
     }
 
     // funzione che restituisce le informazioni dell'utente loggato
