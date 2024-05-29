b73a786eaa8fb6d45acdd43ef2301075c02bb855
samuk52 <samuele.corallo10@gmail.com>
2024-05-29 17:44:14 +0200

    updated HomeActivity: added methods that open fragments in response to the button in the navigation bar pressed

--- app/src/main/java/it/insubria/freerun_runningapp/Activities/HomeActivity.kt
+++ app/src/main/java/it/insubria/freerun_runningapp/Activities/HomeActivity.kt
@@ -2,7 +2,12 @@ package it.insubria.freerun_runningapp.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import it.insubria.freerun_runningapp.Fragments.ActivitiesFragment
+import it.insubria.freerun_runningapp.Fragments.ProfileFragment
+import it.insubria.freerun_runningapp.Fragments.RunFragment
 import it.insubria.freerun_runningapp.R
 
 class HomeActivity : AppCompatActivity() {
@@ -14,5 +19,46 @@ class HomeActivity : AppCompatActivity() {
         bottomNavigationBar.itemActiveIndicatorColor = getColorStateList(R.color.white)
         bottomNavigationBar.selectedItemId = R.id.item_run
 
+        bottomNavigationBar.setOnItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.item_activities -> {
+                    openActivitiesFragment()
+                    true
+                }
+                R.id.item_run -> {
+                    openRunFragment()
+                    true
+                }
+                R.id.item_profile -> {
+                    openProfileFragment()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // metodo che apre l'activities fragment
+    private fun openActivitiesFragment(){
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<ActivitiesFragment>(R.id.fragmentContainerView)
+        }
+    }
+
+    // metodo che apre il run fragments
+    private fun openRunFragment(){
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<RunFragment>(R.id.fragmentContainerView)
+        }
+    }
+
+    // metodo che apre il profile fragments
+    private fun openProfileFragment(){
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<ProfileFragment>(R.id.fragmentContainerView)
+        }
     }
 }
