d182139bf99443136e339cb0bd96dfa53b34e2fe
samuk52 <samuele.corallo10@gmail.com>
2024-05-30 18:30:47 +0200

    added method that takes care of periodically updating the textViews in the custom notification layout

--- app/src/main/java/it/insubria/freerun_runningapp/Services/TrackingService.kt
+++ app/src/main/java/it/insubria/freerun_runningapp/Services/TrackingService.kt
@@ -3,24 +3,26 @@ package it.insubria.freerun_runningapp.Services
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Binder
 import android.os.Build
+import android.os.Handler
 import android.os.IBinder
-import android.view.LayoutInflater
+import android.os.Looper
 import android.widget.RemoteViews
-import android.widget.TextView
 import androidx.core.app.NotificationCompat
 import com.google.android.gms.maps.model.LatLng
+import it.insubria.freerun_runningapp.Activities.TrackingActivity
 import it.insubria.freerun_runningapp.R
 import it.insubria.freerun_runningapp.TrackingRunComponents.CaloriesCounter
 import it.insubria.freerun_runningapp.TrackingRunComponents.LocationProvider
 import it.insubria.freerun_runningapp.TrackingRunComponents.StepCounter
 import it.insubria.freerun_runningapp.TrackingRunComponents.StopWatch
-import org.w3c.dom.Text
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class TrackingService: Service(){
 
@@ -31,6 +33,9 @@ class TrackingService: Service(){
     private lateinit var locationProvider: LocationProvider
     private lateinit var stepCounter: StepCounter
     private lateinit var caloriesCounter: CaloriesCounter
+
+    private lateinit var notificationView: RemoteViews
+    private lateinit var notificationDataUpdater: ExecutorService
 
     class TrackingBinder(private val trackingService: TrackingService): Binder(){
         fun getService(): TrackingService{
@@ -67,6 +72,7 @@ class TrackingService: Service(){
         if(stepCounter.isStepSensorPresent()) {
             stepCounter.start() // se è presente lo avvio
         }
+        startNotificationDataUpdater() // avvio il thread che si occuperà di aggiorna i dati (km e tempo) presenti nella notifica
     }
 
     // metodo che mette in pausa il monitoraggio della corsa.
@@ -77,6 +83,7 @@ class TrackingService: Service(){
         if(stepCounter.isStepSensorPresent()) {
             stepCounter.pause()
         }
+        stopNotificationDataUpdater() // fermo il thread incaricato di aggiornare i dati delle notifiche
     }
 
     // metodo che termina il monitoraggio della corsa.
@@ -87,6 +94,7 @@ class TrackingService: Service(){
         if(stepCounter.isStepSensorPresent()) {
             stepCounter.stop()
         }
+        stopNotificationDataUpdater() // fermo il thread incaricato di aggiornare i dati delle notifiche
     }
 
     // funzione che restituisce il tempo del cronometro.
@@ -149,21 +157,59 @@ class TrackingService: Service(){
     private fun showNotification(){
 
         /*
-        TODO: 1. aggiungere peding intent (intent che viene aperto quando si clicca sulla notifica)
-              2. aggiungere thread che modifica i dati del layout customizzato delle notifiche
+        TODO: 1. aggiungere peding intent (intent che viene aperto quando si clicca sulla notifica
          */
 
         // recupero il layout delle notifiche
-        val contentView = RemoteViews(packageName, R.layout.notification_layout)
+        notificationView = RemoteViews(packageName, R.layout.notification_layout)
+        // imposto il testo delle textView presenti nel layout personalizzato
+        notificationView.setTextViewText(R.id.distanceNotificationText, String.format("%.2f", getKilometres()))
+        notificationView.setTextViewText(R.id.timeNotificationText, getTime())
+
+        // creo il pending intent
+        val notificationIntent = Intent(this, TrackingActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0 or PendingIntent.FLAG_IMMUTABLE)
 
         notification = NotificationCompat.Builder(this, CHANNEL_ID)
             //.setContentTitle("Traking service") // todo modificare
             //.setContentText("Free run tracking your activity") // todo modificare
             .setSmallIcon(R.drawable.run)
-            .setCustomContentView(contentView) // importo il layout customizzato
+            .setCustomContentView(notificationView) // importo il layout customizzato
+            .setContentIntent(pendingIntent)
             .setOnlyAlertOnce(true) // la notifica emette un suono solo la prima volta.
             .build()
         notificationManager.notify(NOTIFY_ID, notification) // mostro la notifica
+    }
+
+    // metodo che fa partire un thread incaricato di aggiornare le textView
+    // presenti nel layout persoanlizzato delle notifiche
+    private fun startNotificationDataUpdater(){
+        notificationDataUpdater = Executors.newSingleThreadExecutor()
+        notificationDataUpdater.execute {
+            //DEBUG
+            println("start notificationUpdater")
+            while (true) {
+                try {
+                    Handler(Looper.getMainLooper()).post {
+                        showNotification()
+                    }
+                    // aggiorno i dati ogni secondo, in questo caso un pò meno di un secondo perchè
+                    // se gli aggiotno ogni secondo preciso, ho notato una inconistenza
+                    Thread.sleep(850)
+                } catch (e: InterruptedException) {
+                    break
+                }
+            }
+            //DEBUG
+            println("stop notificationUpdater")
+        }
+    }
+
+    // metodo che ferma il thread incaricato di aggiornare le textView del layout personalizzato delle notifiche.
+    private fun stopNotificationDataUpdater(){
+        //DEBUG
+        println("--- try to stop notificationUpdater ---")
+        notificationDataUpdater.shutdownNow()
     }
 
     companion object{
